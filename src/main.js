5b957af24c617e22a843833829f3733c4dfdb02a
ChengZi <xn970219@163.com>
2020-05-11 16:56:12 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+// 引入资源 css js
+// 引入vue框架 根组件 及 路由配置 插件
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  // 实例化根实例 render（）渲染app组件 挂载到#app的容器内
   render: h => h(App)
 }).$mount('#app')
